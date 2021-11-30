151e20b1a3ea02079abb1aa269e489e9b8920389
Érick Firmo <erickfirmo1996@gmail.com>
2021-11-30 18:37:33 -0300

    Update PostFactory

--- v6/database/factories/PostFactory.php
+++ v6/database/factories/PostFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Model::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->words(2, true),
+        'description' => $faker->sentence,
     ];
 });
